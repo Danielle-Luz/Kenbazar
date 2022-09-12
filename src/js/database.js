cb2f2092630a5c1ff6196227850ea28686cfaaa5
Axionauta <daniln2209@gmail.com>
2022-09-12 01:26:35 -0300

    remoção e adição no carrinho

--- src/js/database.js
+++ src/js/database.js
@@ -1,9 +1,9 @@
 // Banco de dados dos produtos
-
-const data = [
+export const data = [
   {
     id: 1,
     img: "src/img/notebooks/aspire-5.jpg",
+    alt: "notebook cinza com teclado preto e descanso de tela com detalhes verdes",
     nome: "Notebook Acer Aspire 5",
     descricao:
       "Os notebooks da linha Aspire 5 são convenientemente portáteis e elegantes para acompanhar suas tarefas do dia a dia. Os recursos tecnológicos de sua confiança ao seu lado sempre que precisar. Desempenho suficiente para todas as tarefas, com Processador Intel Core 10ª geração. Você cria, edita, compartilha e assiste vídeos, fotos e imagens sem dificuldades.",
@@ -14,6 +14,7 @@ const data = [
   {
     id: 2,
     img: "src/img/notebooks/asus-ryzen-5.jpg",
+    alt: "notebook cinza com bolhas de sabão com descanso de tela",
     nome: "Notebook Asus AMD Ryzen 5-3500U",
     descricao:
       "O ASUS M515 possui armazenamento SSD de 256GB, que além de ser muito mais rápido que o HD convencional, é menor, mais leve e não tem partes mecânicas extremamente sensíveis a impactos e solavancos. Isso garante maior proteção ao dados armazenados no seu notebook, para que você possa trabalhar sem preocupações, com alto desempenho e produtividade, mesmo em um veículo em movimento.",
@@ -24,6 +25,7 @@ const data = [
   {
     id: 3,
     img: "src/img/notebooks/samsung-i5.jpg",
+    alt: "notebook cinza escuro com machas de tintas roxas e amarelas como descanso de tela",
     nome: "Notebook Samsung Intel Core i5-1135G7",
     descricao:
       "O Samsung Book possui uma arquitetura de última geração e design elegante para quem busca qualidade, desempenho e proteção do investimento. Conta com portas de acesso à memória e unidade de armazenamento, facilitando o upgrade e proporcionando mais longevidade ao sistema. A familia Samsung Books traz uma variedade de configurações, atendendo aos mais diversos perfis de utilização, seja para estudo, trabalho ou uso geral.",
@@ -34,6 +36,7 @@ const data = [
   {
     id: 4,
     img: "src/img/pc/facil.jpg",
+    alt: "gabinete cinza escuro de lado",
     nome: "Computador Fácil Intel Core i5-3470",
     descricao:
       "Seja para grandes ou pequenas empresas, a Linha de Computador Fácil é completamente pensada para atender às necessidades de profissionais que buscam eficiência e bom desempenho na execução de seus trabalhos com excelente custo benefício.",
@@ -45,6 +48,7 @@ const data = [
     id: 5,
     img: "src/img/pc/hp.jpg",
     nome: "Computador HP 280 G5 SFF",
+    alt: "gabinete preto de frente",
     descricao:
     "Para uso pessoal e profissional, o computador HP 280 G5 SFF concilia um excelente desempenho com um baixo custo, seu principal objetivo é ser versátil e útil para qualquer fim, executando com excelência quaisquer tarefas.",
     especificacoes: "i3-10100, 4GB RAM DDR4, SSD 256GB, FreeDOs, Kit Teclado e Mouse - 48T03LA#AK4",
@@ -55,6 +59,7 @@ const data = [
     id: 6,
     img: "src/img/pc/le-novo.jpg",
     nome: "Computador Lenovo V50S",
+    alt: "gabinete preto de lado",
     descricao:
       "Seja para trabalho ou lazer, o Lenovo V50S é um computador que oferece desempenho poderoso e versatilidade. É equipado com processador I3 10100 e memória RAM de 4 GB.",
     especificacoes: "Intel Core i3-10100, 4GB RAM, HD 500GB, Windows 10 Pro, Kit teclado e Mouse - 11HA000QBP",
@@ -64,6 +69,7 @@ const data = [
   {
     id: 7,
     img: "src/img/monitores/lg-led-25.jpg",
+    alt: "Monitor preto com personagens de jogo armados como descanso de tela",
     nome: "Monitor Gamer LG 25' IPS",
     descricao:
       "A proporção da imagem do monitor UltraWide torna os jogos e filmes mais envolventes do que nunca. A nitidez da resolução Full HD de 1080P com IPS faz a diferença. Simplificando, de qualquer ângulo de visão, tudo fica mais nítido e detalhado em FULL HD.",
@@ -74,6 +80,7 @@ const data = [
   {
     id: 8,
     img: "src/img/monitores/lg-led-23.jpg",
+    alt: "monitor cinza com balões coloridos como descanso de tela",
     nome: "Monitor LG 23.8' IPS",
     descricao:
     "O novo Monitor LG 23,8 conta com tecnologia IPS para melhor reprodução de cores e maior ângulo de visão, proporcionando imagens brilhantes que surgem com detalhes vibrantes de qualquer ângulo de visualização. Combinada com resolução 1080p Full HD (1920x1080) para melhor definição.",
@@ -84,6 +91,7 @@ const data = [
   {
     id: 9,
     img: "src/img/monitores/AOC.jpg",
+    alt: "monitor preto com prédios como descanso de tela",
     nome: "Monitor AOC 23.8' LED",
     descricao:
       "Com o Monitor AOC Série B1 24B1XHM você poderá desfrutar de conteúdos multimídia com mais imersão e aumentar sua performance em qualquer tarefa. Super completo e moderno, ele possui tela widescreen LED de 23.8 com resolução full HD e painel VA, proporcionando imagens incríveis, detalhes nítidos e cores vibrantes.",
@@ -94,6 +102,7 @@ const data = [
   {
     id: 10,
     img: "src/img/headsets/microsoft.jpg",
+    alt: "headset preto com microfone",
     nome: "Headset Microsoft LifeChat LX3000",
     descricao:
       "O Headset LifeChat da Microsoft oferece diferentes recursos para ouvir músicas e conversar. Com alto-falantes almofadados e revestidos em PU, proporciona conforto e qualidade no som digital. Possui também total integração com o Windows Live Messenger e fácil instalação pela porta USB.",
@@ -104,6 +113,7 @@ const data = [
   {
     id: 11,
     img: "src/img/headsets/rydo.jpg",
+    alt: "headset preto com microfone",
     nome: "Headset Rydo Trust On-Ear",
     descricao:
     "O Trust Rydo está aí para ajudar. Este fone de ouvido compacto é feito para ser usado confortavelmente, com um microfone ajustável para ajudá-lo a se comunicar. Fácil de usar, com ótima qualidade de som; adequado para todos os aplicativos de chat (vídeo), como Skype, Teams ou Zoom.",
@@ -114,6 +124,7 @@ const data = [
   {
     id: 12,
     img: "src/img/headsets/logitech.jpg",
+    alt: "headset preto com microfone",
     nome: "Headset com fio Logitech",
     descricao:
       "Experimente chamadas de voz, Skype, webinars e muito mais com clareza usando uma conexão USB plug-and-play simples com o headset H390. O microfone rígido do lado esquerdo pode ser colocado na posição mais adequada para capturar melhor sua voz e é móvel podendo ser recolhido para não atrapalhar quando não estiver sendo usado.",
@@ -124,6 +135,7 @@ const data = [
   {
     id: 13,
     img: "src/img/mouses/redragon.jpg",
+    alt: "mouse preto com luzes verdes, amarelas e vermelhas nas extremidades",
     nome: "Mouse Gamer Redragon Cobra",
     descricao:
       "O Mouse Gamer Redragon Cobra, possui sensor PIXART 3327 para Alta Performance (20 G / 100 IPS) e seu sistema é de Iluminação RGB Redragon Chroma Mk.II (7 Diferentes Modos de Iluminação). Possui Polling Rate de 1000hz (Tempo de Resposta Ajustável via Software de 1/2/4/8ms), com 8 Botões Programáveis e 2 Botões laterais para funções / atalhos, macros, etc.",
@@ -134,6 +146,7 @@ const data = [
   {
     id: 14,
     img: "src/img/mouses/redragon-lunar.jpg",
+    alt: "mouse branco com luzes azuis, roxas, rosas e verdes nas extremidades",
     nome: "Mouse Gamer Redragon Cobra Lunar White",
     descricao:
     "Trazendo a já amada iluminação Redragon Chroma Mark II o mouse Gamer Profissional Cobra impressiona com tanta beleza, conforto e precisão. Vá além e customize do seu jeito a iluminação do seu mouse com o software Redragon.",
@@ -145,6 +158,7 @@ const data = [
     id: 15,
     img: "src/img/mouses/logitech.jpg",
     nome: "Mouse Gamer Sem Fio Logitech",
+    alt: "mouse preto com círculo azul iluminado na parte inferior",
     descricao:
       "Projetado em colaboração direta de muitos jogadores profissionais de e-sports, o mouse gamer PRO Wireless foi desenvolvido de acordo com os exigentes padrões de alguns dos maiores profissionais de e-sports do mundo. O PRO Wireless gaming mouse foi concebido para oferecer um desempenho de ponta e inclui as mais recentes e avançadas tecnologias disponíveis.",
     especificacoes: "Sem Fio, Wireless LIGHTSPEED, RGB LIGHTSYNC, Ambidestro, 6 Botões Programáveis, HERO 25K - 910-005271",
@@ -156,6 +170,7 @@ const data = [
     id: 16,
     img: "src/img/teclados/logitech.jpg",
     nome: "Teclado Logitech ERGO K860",
+    alt: "teclado preto e curvo com formato ergonômico",
     descricao:
       "Digite mais naturalmente com o ERGO K860 - um teclado ergonômico avançado que promove uma postura de digitação mais relaxada - reduzindo a tensão do pulso em 25% e oferecendo 54% mais suporte para o pulso.",
     especificacoes: "Layout Dividido, Suporte para Descanso de Pulso, Conexão Bluetooth e USB, Windows e Mac",
@@ -165,6 +180,7 @@ const data = [
   {
     id: 17,
     img: "src/img/teclados/microsoft.jpg",
+    alt: "teclado preto e curvo com formato ergonômico",
     nome: "Teclado Microsoft Comfort",
     descricao:
     "Com o Microsoft Wireless Keyboard, aproveite um wireless comfort desktop com estilo e segurança. O teclado curvado ergonomicamente aprovado encoraja você a assumir uma postura mais natural que alinha seus braços, pulsos e mãos, oferecendo maior conforto.",
@@ -175,6 +191,7 @@ const data = [
   {
     id: 18,
     img: "src/img/teclados/microsoft-sculpt.jpg",
+    alt: "teclado preto e curvo com formato ergonômico e mouse com touchpad a direita",
     nome: "Teclado Microsoft Sculpt",
     descricao:
       "O descanso de palma acolchoado oferece suporte e promove uma posição neutra do pulso. O design de teclado dividido ajuda a posicionar os pulsos e antebraços em uma posição natural e relaxada. O layout da chave de arco natural imita a forma curvada das pontas dos dedos. O design de teclado abobadado posiciona os pulsos em um ângulo natural e relaxado.",
--- src/js/index.js
+++ src/js/index.js
@@ -1,58 +1,135 @@
+import { data } from "./database.js";
 
+//elementos do dom
 const vitrine = document.querySelector (".vitrine");
 const carrinhoAdicionados = document.querySelector (".carrinho-adicionados");
+const carrinhoVazio = document.querySelector (".carrinho-vazio");
+const bLinks = document.querySelectorAll (".links>li>button");
+const todosBotoesHeader = document.querySelectorAll (".links button");
+const hamburguerMenu = document.querySelector (".hamburguer-menu");
 
-const bComputadores = document.getElementById ("b-computadores");
-const bHardware = document.getElementById ("b-hardware");
-const bPerifericos = document.getElementById ("b-perifericos");
-const bLinks = document.querySelectorAll (".links button");
-
-const filtros = JSON.parse (localStorage.getItem ("filtros"));
-const filtroNaoExiste = !filtros;
+//obtenção de filtro ativos
+const filtro = JSON.parse (localStorage.getItem ("filtro"));
+//tentando converter falsy value em true
+const filtroNaoExiste = !filtro;
 if (filtroNaoExiste) {
-    localStorage.setItem ("filtros", JSON.stringify ([]));
+    //setando lista vazia no local storage caso não exista
+    localStorage.setItem ("filtro", JSON.stringify (""));
 }
 
+//fazendo cópia da lista de produtos
 let listaProdutos = data.slice (0);
 
-bComputadores.onclick = () => {
-    localStorage.setItem ("filtros", JSON.stringify (["Computadores"]));
+//filtragem com base no botão clicado
+for (let botao of todosBotoesHeader) {
+    botao.onclick = () => {
+        let filtro = botao.innerText != "Todos" ? botao.innerText : "";
+        localStorage.setItem ("filtro", JSON.stringify (filtro));
+        window.location.reload ();
+    }
+}
 
-    window.location.reload ();
-};
+//exibição do dropdown
+for (let botao of bLinks) {
+    if (botao.innerText != "Todos") {
+        //capturando dropdown do botão
+        const subcategorias = botao.closest ("li").querySelector (".links-subcategorias");
+        botao.onmouseenter = () => {
+            //tornando dropdown visível
+            subcategorias.classList.remove ("hidden");
+            //adicionando visual que indica a qual botão pertence o dropdown
+            botao.classList.add ("dropdown-ativo");
+        };
+        //tornando dropdown invisível
+        botao.onmouseleave = () => {
+            subcategorias.classList.add ("hidden");
+        }
+        //retirando estilização do botão quando o mouse sair de cima do dropdown ou dele
+        botao.closest ("li").onmouseleave = () => {
+            botao.classList.remove ("dropdown-ativo");
+        };
+    }
+}
 
-bHardware.onclick = () => {
-    localStorage.setItem ("filtros", JSON.stringify (["Hardware"]));
+hamburguerMenu.addEventListener ("click", () => {
+    const nav = document.querySelector ("nav");
+    nav.style.display = nav.style.display == "flex" ? "none" : "flex";
+    hamburguerMenu.classList.toggle ("rotacionar");
+});
 
-    window.location.reload ();
-};
-
-bPerifericos.onclick = () => {
-    localStorage.setItem ("filtros", JSON.stringify (["Periféricos"]));
-    
-    window.location.reload ();
-};
-
-listaProdutos = filtrarElementos (filtros, data);
-
+listaProdutos = filtrarElementos (filtro, data);
 renderizarProdutos (listaProdutos, criarProdutoVitrine);
 
+const bAdicionarCarrinho = document.querySelectorAll (".b-adicionar");
+console.log (bAdicionarCarrinho)
 
-function filtrarElementos (filtros, lista) {
-    let copiaLista = lista.splice (0);
-    if (!filtros) {
+//adicionar ao carrinho
+for (let botao of bAdicionarCarrinho) {
+    botao.addEventListener ("click", () => {
+        const id = parseInt (botao.closest (".produto").id.split ("_")[1]) - 1;
+        const estaNoCarrinho = carrinhoAdicionados.querySelector (`#carrinho_${id + 1}`);
+        if (!estaNoCarrinho) {
+            const produto = data[id];
+            if (!localStorage.getItem ("carrinho")) {
+                localStorage.setItem ("carrinho", JSON.stringify ([]));
+            }
+            const carrinho = JSON.parse (localStorage.getItem ("carrinho"));
+            carrinho.push (produto);
+            localStorage.setItem ("carrinho", JSON.stringify (carrinho));
+            criarProdutoCarrinho (produto);
+            mostraCarrinho ();
+        }
+    });
+}
+
+carrinhoAdicionados.innerHTML = "";
+const carrinho = JSON.parse (localStorage.getItem ("carrinho"));
+renderizarProdutos (carrinho, criarProdutoCarrinho);
+const botoesRemover = document.querySelectorAll (".b-remover");
+
+for (let botao of botoesRemover) {
+    botao.addEventListener ("click", () => {
+        const produto = botao.closest (".produto");
+        const id = parseInt (produto.id.split ("_")[1]);
+        const carrinho = JSON.parse (localStorage.getItem ("carrinho"));
+        const posicao = carrinho.findIndex (produto => {
+            return produto.id == id;
+        });
+        carrinho.splice (posicao, 1);
+        localStorage.setItem ("carrinho", JSON.stringify (carrinho));
+        produto.remove ();
+        if (carrinhoAdicionados.querySelectorAll ("figure").length == 0) {
+            carrinhoVazio.classList.remove ("hidden");
+            carrinhoAdicionados.parentNode.closest ("section").classList.add ("hidden");
+        }
+    });
+}
+
+function mostraCarrinho () {
+    if (carrinhoAdicionados.querySelectorAll ("figure").length > 0) {
+        carrinhoVazio.classList.add ("hidden");
+        carrinhoAdicionados.parentNode.closest ("section").classList.remove ("hidden");
+    }
+}
+
+mostraCarrinho ();
+
+
+function filtrarElementos (filtro, lista) {
+    //caso não haja filtro, retorna a lista sem filtragem
+    if (!filtro) {
         return lista;
     } else {
-        for (let filtro of filtros) {
-            copiaLista = copiaLista.filter (produto => {
-                return produto.categorias.includes (filtro);
-            });
-        }
-        return copiaLista;
+        lista = lista.filter (produto => {
+            //só inclui o produto na lista se o filtro atual for uma das suas categorias
+            return produto.categorias.includes (filtro);
+        });
+        return lista;
     }
 }
 
 function renderizarProdutos (lista, funcaoCriacao) {
+    console.log (lista)
     for (let produto of lista) {
         funcaoCriacao (produto);
     }
@@ -63,10 +140,9 @@ function criarProdutoVitrine (produto) {
     const id = `vitrine_${produto.id}`;
     figure.className = "produto";
     figure.id = id;
-    console.log (figure.id);
     figure.innerHTML = `
     <div class="div-imagem">
-        <img src=${produto.img} alt="">
+        <img src="${produto.img}" alt="${produto.alt}" width="100" height="100">
     </div>
     <figcaption>
         <ul class="lista-categorias">
@@ -74,12 +150,10 @@ function criarProdutoVitrine (produto) {
         <h2 class="nome-produto">${produto.nome}</h2>
         <p class="descricao">${produto.descricao}</p>
         <span class="preco">R$ ${produto.valor.toFixed (2)}</span>
-        <button>Adicionar ao carrinho</button>
+        <button class="b-adicionar">Adicionar ao carrinho</button>
     </figcaption>`;
-
     vitrine.appendChild (figure)
     figure = document.getElementById (id);
-    console.log (figure);
     const listaCategorias = figure.querySelector (".lista-categorias");
     const categoriasProduto = produto.categorias;
     for (let categoria of categoriasProduto) {
@@ -88,46 +162,17 @@ function criarProdutoVitrine (produto) {
 }
 
 function criarProdutoCarrinho (produto) {
-    return `
-    <figure class="produto"  id="carrinho_${produto.id}">
-        <div class="div-imagem">
-            <img src=${produto.img} alt="">
+    const figure = document.createElement ("figure");
+    figure.className = "produto";
+    figure.id = `carrinho_${produto.id}`;
+    figure.innerHTML = `
+    <div class="div-imagem">
+        <img src="${produto.img}" alt="${produto.alt}">
         </div>
-        <figcaption>
-            <h4 class="nome-produto">${produto.nome}</h4>
-            <span class="preco">R$ ${produto.valor.toFixed (2)}</span>
-            <button>Remover produto</button>
-        </figcaption>
-    </figure>`;
+    <figcaption>
+        <h4 class="nome-produto">${produto.nome}</h4>
+        <span class="preco">R$ ${produto.valor.toFixed (2)}</span>
+        <button class="b-remover">Remover produto</button>
+    </figcaption>`
+    carrinhoAdicionados.appendChild (figure);
 }
-
-//PRODUTO NA VITRINE
-/*
-<figure class="produto">
-    <div class="div-imagem">
-        <img src="src\img\camiseta_preta.svg" alt="">
-    </div>
-    <figcaption>
-        <small class="categoria">Camisetas</small>
-        <h2 class="nome-produto">Lightweight Jacket</h2>
-        <p class="descricao">Adicione um pouco de energia ao seu guarda-roupa de inverno com esta jaqueta vibrante esta jaqueta vibrante</p>
-        <span class="preco">R$ 100.00</span>
-        <button>Adicionar ao carrinho</button>
-    </figcaption>
-</figure>
-*/
-
-//PRODUTO NO CARRINHO
-/*
-<figure class="produto">
-    <div class="div-imagem">
-        <img src="src\img\camiseta_preta.svg" alt="">
-    </div>
-    <figcaption>
-        <h4 class="nome-produto">Lightweight Jacket</h4>
-        <span class="preco">R$ 100.00</span>
-        <button>Remover produto</button>
-    </figcaption>
-</figure>
-
-*/
